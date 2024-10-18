f18de3d9b16a716965045ba54cf39d260a13a5eb
danmesfin <danielmsfn@gmail.com>
2024-10-18 17:23:18 +0300

    fix: deploy issues

--- server/app.py
+++ server/app.py
@@ -24,7 +24,7 @@ with app.app_context():
     db.create_all()
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
 from flask import jsonify
 from models import User
