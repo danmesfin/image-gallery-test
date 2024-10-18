ea582a265583767ca8e0a7d24a937095befe543d
danmesfin <danielmsfn@gmail.com>
2024-10-18 16:58:09 +0300

    updated configs for image upload handling

--- server/image.py
+++ server/image.py
@@ -1,8 +1,14 @@
+import uuid
 import boto3
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import Image, db, User
 from config import Config
+from supabase import create_client, Client
+from config import Config
+
+supabase: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY)
+
 
 image_bp = Blueprint('image', __name__)
 s3 = boto3.client(
@@ -14,20 +20,61 @@ s3 = boto3.client(
 @image_bp.route('/upload', methods=['POST'])
 @jwt_required()
 def upload_image():
+    if not Config.S3_BUCKET:
+        return jsonify(message="S3 bucket not configured"), 500
+
     user_id = get_jwt_identity()
+    if 'file' not in request.files:
+        return jsonify(message="No file part"), 400
     file = request.files['file']
-    s3.upload_fileobj(file, Config.S3_BUCKET, file.filename)
-
-    image = Image(filename=file.filename, user_id=user_id)
-    db.session.add(image)
-    db.session.commit()
-
-    return jsonify(message="Image uploaded successfully"), 200
+    if file.filename == '':
+        return jsonify(message="No selected file"), 400
+    if file:
+        filename = str(uuid.uuid4()) + '_' + file.filename
+        s3.upload_fileobj(file, Config.S3_BUCKET, filename)
+        image = Image(filename=filename, user_id=user_id)
+        db.session.add(image)
+        db.session.commit()
+        return jsonify(message="Image uploaded successfully", filename=filename), 200
 
 @image_bp.route('/images', methods=['GET'])
 @jwt_required()
 def get_images():
     user_id = get_jwt_identity()
     images = Image.query.filter_by(user_id=user_id).all()
-    image_urls = [Config.S3_LOCATION + img.filename for img in images]
-    return jsonify(images=image_urls), 200
+    image_data = [{
+        'id': img.id,
+        'filename': img.filename,
+        'url': img.url,
+        'upload_date': img.upload_time.isoformat()
+    } for img in images]
+    return jsonify(images=image_data, count=len(image_data)), 200
+
+@image_bp.route('/upload-supabase', methods=['POST'])
+@jwt_required()
+def upload_image_supabase():
+    user_id = get_jwt_identity()
+    if 'file' not in request.files:
+        return jsonify(message="No file part"), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify(message="No selected file"), 400
+    if file:
+        filename = str(uuid.uuid4()) + '_' + file.filename
+        file_content = file.read()
+        
+        # Upload to Supabase Storage
+        result = supabase.storage.from_(Config.SUPABASE_BUCKET).upload(filename, file_content)
+        
+        if result:
+            # Get the public URL of the uploaded file
+            file_url = supabase.storage.from_(Config.SUPABASE_BUCKET).get_public_url(filename)
+            
+            # Save image info to database
+            image = Image(filename=filename, user_id=user_id, url=file_url)
+            db.session.add(image)
+            db.session.commit()
+            
+            return jsonify(message="Image uploaded successfully", filename=filename, url=file_url), 200
+        else:
+            return jsonify(message="Upload to Supabase failed"), 500
--- server/models.py
+++ server/models.py
@@ -6,11 +6,15 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(120), nullable=False)
+    filename = db.Column(db.String(255), nullable=False)  # Increased from 120 to 255
     upload_time = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('images', lazy=True))
+    url = db.Column(db.String(512), nullable=True)  # Increased to 512 to accommodate longer URLs
+
+    def __repr__(self):
+        return f'<Image {self.filename}>'
