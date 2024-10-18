ddba6b747e74293c4b5fcc4bcaf26fe22552e238
danmesfin <danielmsfn@gmail.com>
2024-10-18 16:57:28 +0300

    feat: auth context

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useState, ReactNode } from "react";
 import axios from "axios";
 
 interface AuthContextProps {
   isAuthenticated: boolean;
+  token: string | null;
   login: (username: string, password: string) => Promise<void>;
   register: (username: string, password: string) => Promise<void>;
   logout: () => void;
@@ -12,11 +14,17 @@ const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [token, setToken] = useState<string | null>(null);
 
   const login = async (username: string, password: string) => {
     try {
-      const response = await axios.post("/auth/login", { username, password });
-      localStorage.setItem("token", response.data.access_token);
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/auth/login`,
+        { username, password }
+      );
+      const accessToken = response.data.access_token;
+      setToken(accessToken);
+      localStorage.setItem("token", accessToken);
       setIsAuthenticated(true);
     } catch (error) {
       console.error("Login failed", error);
@@ -25,19 +33,24 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   const register = async (username: string, password: string) => {
     try {
-      await axios.post("/auth/register", { username, password });
+      await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/register`, {
+        username,
+        password,
+      });
     } catch (error) {
       console.error("Registration failed", error);
     }
   };
-
   const logout = () => {
     localStorage.removeItem("token");
+    setToken(null);
     setIsAuthenticated(false);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, register, logout }}>
+    <AuthContext.Provider
+      value={{ isAuthenticated, token, login, register, logout }}
+    >
       {children}
     </AuthContext.Provider>
   );
