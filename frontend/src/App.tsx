5a27daccabdf65fb4e132b4baf0f9a0673455e65
danmesfin <danielmsfn@gmail.com>
2024-10-18 16:57:33 +0300

    feat: auth context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,70 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { LoginForm } from "./components/LoginForm";
 import { RegisterForm } from "./components/RegisterForm";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 import LandingPage from "./Pages/LandingPage";
 import Dashboard from "./Pages/Dashboard";
 import UploadImage from "./Pages/UploadImage";
 import MyImages from "./Pages/MyImages";
+
+const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+const AppRoutes = () => {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <Routes>
+      <Route
+        path=""
+        element={
+          isAuthenticated ? <Navigate to="/dashboard" /> : <LandingPage />
+        }
+      />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/upload"
+        element={
+          <ProtectedRoute>
+            <UploadImage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/myimages"
+        element={
+          <ProtectedRoute>
+            <MyImages />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+};
+
 const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <div className="container mx-auto">
-          <Routes>
-            <Route path="" element={<LandingPage />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterForm />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/upload" element={<UploadImage />} />
-            <Route path="/myimages" element={<MyImages />} />
-          </Routes>
+        <div className="">
+          <AppRoutes />
         </div>
       </Router>
     </AuthProvider>
