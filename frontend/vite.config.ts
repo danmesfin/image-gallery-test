41b1aad97237c52b86d17f20a253cde38496233b
danmesfin <danielmsfn@gmail.com>
2024-10-18 17:54:48 +0300

    fix deploy issues on vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,9 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  server: {
+    port: process.env.PORT as unknown as number,
   },
+  plugins: [vercel()],
 });
